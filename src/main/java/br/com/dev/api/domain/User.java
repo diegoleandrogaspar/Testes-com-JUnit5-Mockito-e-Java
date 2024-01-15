6b0568563f804aadd56967e61316d61f6a33e1dc
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-01-14 22:44:59 -0300

    classe usuario

---
+++ src/main/java/br/com/dev/api/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.dev.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
