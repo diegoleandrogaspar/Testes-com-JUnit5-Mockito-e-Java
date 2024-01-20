2673cb10496867d66f925bc840cd9e289f53c20c
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-01-19 21:47:05 -0300

    Perfil de teste

---
+++ src/main/java/br/com/dev/api/config/LocalConfig.java
@@ -0,0 +1,28 @@
+package br.com.dev.api.config;
+
+import br.com.dev.api.domain.User;
+import br.com.dev.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    public void starDB() {
+        User u1 = new User(null, "diego", "diego@gmail.com", "123");
+        User u2 = new User(null, "luiz", "luiz@gmail.com", "456");
+
+        userRepository.saveAll(List.of(u1,u2));
+
+
+
+    }
+}
