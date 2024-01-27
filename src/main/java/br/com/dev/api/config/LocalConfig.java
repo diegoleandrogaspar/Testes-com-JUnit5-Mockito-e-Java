b708c9d9d3c1d834cafd4736983da27d8231b0b7
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-01-26 21:32:12 -0300

    teste

--- src/main/java/br/com/dev/api/config/LocalConfig.java
+++ src/main/java/br/com/dev/api/config/LocalConfig.java
@@ -19,8 +19,9 @@ public class LocalConfig {
     public void starDB() {
         User u1 = new User(null, "diego", "diego@gmail.com", "123");
         User u2 = new User(null, "luiz", "luiz@gmail.com", "456");
+        User u3 = new User(null, "silva", "silva@gmail.com", "789");
 
-        userRepository.saveAll(List.of(u1,u2));
+        userRepository.saveAll(List.of(u1,u2,u3));
 
 
 
